561a660ba623780b3df1959ecab572de1bd12774
Progosling <progosling@gmail.com>
2024-01-24 08:48:05 +0300

    Updated cover image

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -3,7 +3,6 @@ import DateFormatter from "./date-formatter";
 import CoverImage from "./cover-image";
 import PostTitle from "./post-title";
 import type Author from "../interfaces/author";
-import nextConfig from "../next.config";
 
 type Props = {
   title: string;
@@ -20,10 +19,7 @@ const PostHeader = ({ title, coverImage, date, author }: Props) => {
         <Avatar name={author.name} picture={author.picture} />
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage
-          title={title}
-          src={`${nextConfig.basePath ?? ""}${coverImage}`}
-        />
+        <CoverImage title={title} src={coverImage} />
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
